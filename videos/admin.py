dcfd1e8f35b198c4fce8ef768ce206006e256777
suyogkokaje <suyogkokaje6@gmail.com>
2024-02-04 02:14:07 +0530

    feat:displaying videos in admin dashboard

--- videos/admin.py
+++ videos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Video
+
+class VideoAdmin(admin.ModelAdmin):
+    ordering = ('-published_at',)
+    list_display = ('video_id','title','published_at')
+
+admin.site.register(Video,VideoAdmin)
