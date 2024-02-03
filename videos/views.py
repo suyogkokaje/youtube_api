bfeb49c32192b89cc5b4eb9d8bd7021326edd503
suyogkokaje <suyogkokaje6@gmail.com>
2024-02-04 02:00:43 +0530

    feat:params to filter published_at field

--- videos/views.py
+++ videos/views.py
@@ -17,4 +17,13 @@ class VideoViewSet(viewsets.ReadOnlyModelViewSet):
 
     @method_decorator(cache_page(60 * 15, cache='default'))
     def list(self, request, *args, **kwargs):
+        queryset = Video.objects.all().order_by('-published_at')
+
+        start_date = request.query_params.get('start_date', None)
+        end_date = request.query_params.get('end_date', None)
+
+        if start_date and end_date:
+            queryset = queryset.filter(published_at__range=[start_date, end_date])
+
+        self.queryset = queryset
         return super().list(request, *args, **kwargs)
