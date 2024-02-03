d4d90ddbd73bb44d2b5ab35886c53905b31df319
suyogkokaje <suyogkokaje6@gmail.com>
2024-02-04 01:48:44 +0530

    feat:redis caching

--- videos/views.py
+++ videos/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from .models import Video
 from .serializers import VideoSerializer
 
@@ -12,3 +14,7 @@ class VideoViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Video.objects.all().order_by('-published_at')
     serializer_class = VideoSerializer
     pagination_class = VideoPagination
+
+    @method_decorator(cache_page(60 * 15, cache='default'))
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
